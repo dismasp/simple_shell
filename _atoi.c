4876ca135ff776483e766c7d95822af9a1fc9a3a
Dismas Albert <dismasp@gmail.com>
2023-04-27 10:18:30 -0700

    Atoi

--- _atoi.c
+++ _atoi.c
@@ -1,19 +1,16 @@
 #include "shell.h"
 
 /**
- * is_interactive - returns true if shell is in interactive mode
- * @info: struct address
- * Return: 1 if interactive mode, 0 otherwise
- */
+*is_interactive - returns true if shell is in interactive mode
+*@info: struct address
+*
+*Return: 1 if interactive mode, 0 otherwise
+*/
 
 int is_interactive(info_t *info)
+
 {
-return (isatty(STDIN_FILENO)
-& amp;
-&amp;
-info - &gt; readfd
-& lt;
-= 2);
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -32,80 +29,49 @@ return (0);
 }
 
 /**
- *check_alpha - checks for alphabetic character
- *@ch: The character to input
- * Return: 1 if ch is alphabetic, 0 otherwise
- */
+*check_alpha - checks for alphabetic character
+*@ch: The character to input
+*Return: 1 if ch is alphabetic, 0 otherwise
+*/
 
 int check_alpha(int ch)
 {
-
-if ((ch & gt;
-&#39;
-a & #39;
-&amp;
-&amp;
-ch & lt;
-&#39;
-z & #39;)
-|| (ch & gt;
-&#39;
-A & #39;
-&amp;
-&amp;
-ch & lt;
-&#39;
-Z & #39;))
+if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 return (1);
-
 else
 return (0);
 }
+
 /**
- * str_to_int - converts a string to an integer
- *@str: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
- */
+*str_to_int - converts a string to an integer
+*@str: the string to be converted
+*Return: 0 if no numbers in string, converted number otherwise
+*/
+
 int str_to_int(char *str)
 {
-int idx, sign_multiplier = 1,
-check_flag = 0, final_output;
+int idx, sign_multiplier = 1, check_flag = 0, final_output;
 unsigned int num_result = 0;
-for (idx = 0; str[idx] !=
-&#39;
-\0 & #39;
-&amp;
-&amp;
-check_flag != 2;
-idx++)
+
+for (idx = 0; str[idx] != '\0' && check_flag != 2; idx++)
 {
-if (str[idx]
-& #39;
--&#39;)
-sign_multiplier *
--1;
-if (str[idx] & gt;
-&#39;
-0 & #39;
-&amp;
-&amp;
-str[idx]
-& lt;
-&#39;
-9 & #39;)
+if (str[idx] == '-')
+sign_multiplier *= -1;
+
+if (str[idx] >= '0' && str[idx] <= '9')
 {
 check_flag = 1;
 num_result *= 10;
-num_result += (str[idx] -
-&#39;
-0 & #39;)
+num_result += (str[idx] - '0');
 }
 else if (check_flag == 1)
 check_flag = 2;
 }
+
 if (sign_multiplier == -1)
 final_output = -num_result;
 else
 final_output = num_result;
+
 return (final_output);
 }
